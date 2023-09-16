e8b642d82563dbce22f147b08dd7ed3fb2c288b7
Kshitij Kedlaya <137076957+Kshitij8Kedlaya@users.noreply.github.com>
2023-09-16 20:38:19 +0530

    Capstone Project.py

--- Capstone Project.py
+++ Capstone Project.py
@@ -1,3 +1,13 @@
+morse = {'.-':'A','-...':'B','-.-.':'C','-..':'D',
+'.':'E','..-.':'F','--,':'G','....':'H','..':'I',
+'.---':'J','-.-':'K','.-..':'L','--':'M','-.':'N',
+'---':'O','.--.':'P','--.-':'Q','.-.':'R','...':'S',
+'-':'T','..-':'U','...-':'V','.--':'W','-..-':'X',
+'-.--':'Y','--..':'Z','.----':'1','..---':'2',
+'...--':'3','....-':'4','.....':'5','-....':'6',
+'--...':'7','---..':'8','----.':'9','-----':'0',
+}
+
 class decode:
     def __init__(self,message,type):
         self.message = message
@@ -19,13 +29,21 @@ class reverse_cphr(decode):
 class caeser_cphr(decode):
     def msg(self):
         txt = ""
+        dmsg = ''
         for m in self.message:
             if m.isalpha():
                 n = m.isupper()
-                m = m.lower()
-                dmsg = chr((ord(m) - 3))
-                if n:
-                    dmsg = dmsg.upper()
+                o = m.lower()
+                if m == 'a' or m == 'b' or m == 'c':
+                    dmsg = chr((ord(m) + 23))
+                elif n:
+                    m = m.upper()
+                    if m == 'A' or m == 'B' or m == 'C':
+                        dmsg = chr((ord(m) + 23))
+                    else:
+                        dmsg = chr((ord(m) - 3))
+                else:
+                    dmsg = chr((ord(o) - 3))
                 txt += dmsg
             else:
                 txt += m
@@ -51,23 +69,24 @@ class ROT13_cphr(decode):
 
             text += nmsg
         print(text)
-    '''
+
 class Morse_Code(decode):
     def msg(self):
         spl = self.message.split("   ")
         dcode = ''
+        nc = ''
         for j in spl:
             j = j.upper()
-            if j in Moco:
-                k = Moco[j]
+            if j in morse:
+                k = morse[j]
                 nc += k
             else:
                 nc = j
         dcode += nc
         print(dcode)
-    '''
 
-cipher_text = "Gur sbk, dlroW olleH, Kl shrsoh"
+
+#Testcode = "ROT13- Uryyb Jbeyq, Reverse - !uoy teem ot eciN ,Caesar - L oryh Frglqj,Morse = .--.   -.--   -   ....   ---   -."
 
 message = input("Enter the Secret Message: ")
 while message:
@@ -85,10 +104,10 @@ while message:
         code1 = ROT13_cphr(message,type)
         code1.inp() 
         code1.msg()
-        '''
+        
     if type == 'Morse Code':
         code1 = Morse_Code(message,type)
         code1.inp() 
         code1.msg()
     message = input("Enter the Secret Message: ")
-    '''
+    
